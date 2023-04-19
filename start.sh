e8e09dd36d8c779fa9ba24462da211352d38ddfd
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2023-04-19 08:58:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/Appu13vava/xlinkz.git /xlinkz
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /xlinkz
 fi
-cd /URL-Shortener-V2
+cd /xlinkz
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
